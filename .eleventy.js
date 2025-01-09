c1d8e2045b32f4a28795c62e7296c13e520c5c0d
Conor Dempsey <conor@Conors-MacBook-Air-2.local>
2025-01-09 19:44:14 +0000

    11ty skeleton commit

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig) {
+    return {
+        markdownTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dir: {
+            input: 'src',
+            includes: '_includes',
+            output: '_site'
+        }
+    }
+}
---
+++ src/index.md
@@ -0,0 +1,5 @@
+---
+title: 'Abhishek Bhattachrayya'
+layout: 'layouts/home.html'
+---
+A place where I keep my recent photographic projects.
