87bc600bb1fdecac38a8e74dd1bdd838455cd65a
sadhbh <codeberg.gilled733@passinbox.com>
2025-01-11 10:21:16 +0000

    include eleventy-img and navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
+const {eleventyImageTransformPlugin} = require("@11ty/eleventy-img");
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
     return {
         markdownTemplateEngine: 'njk',
         dataTemplateEngine: 'njk',
