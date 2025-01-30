f81514e622893d24616a2824547691725b792fb3
sadhbh <codeberg.gilled733@passinbox.com>
2025-01-30 11:46:32 +0000

    sort resize handling differently for gallery vs scroller pages

--- js/main.js
+++ js/main.js
@@ -3,36 +3,66 @@ const scrollers = document.querySelectorAll('.scroller');
 
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
 
-    function go(){
-        // check if animation has been added
-        let animationAdded = scrollers[0].getAttribute("data-animated");
 
-        // check if the window is wider than 768px
-        if (document.documentElement.clientWidth > 420) {
-            // if so add the animation if it hasn't been added
-            if (animationAdded === "false" || animationAdded === null) {
-                addAnimation();
+    // the resize does two things, depending on whether a scroller is present
+
+    // whenever the screen is small we add the class 'small-screen' to the body
+    // we also add the class yscroll to the body, which allows vertical scrolling
+    // if there are scrollers on the page, we remove the animation from the scroller
+
+    // whenever the screen is large we remove the class 'small-screen' from the body
+    // if we are on a page with scrollers, we remove yscroll from the body
+    // if there are scrollers on the page, we add the animation to the scroller
+
+
+    function resizeHandler(){
+
+        const scrollersPresent = scrollers.length > 0;
+
+        if (document.documentElement.clientWidth < 420) {
+            // add the class 'small-screen' to the body
+            document.querySelector('body').classList.add('small-screen');
+            // add the class 'yscroll' to the body
+            document.querySelector('body').classList.add('yscroll');
+
+            // if we're on a page with scrollers, check if animation has been added
+            // if so remove the animation
+            if (scrollersPresent) {
+                let animationAdded = scrollers[0].getAttribute("data-animated");
+                if (animationAdded === "true") {
+                    removeAnimation();
+                };
             };
+        } else { // case for large screens
+            // if scrollers are present 
+            if (scrollersPresent) {
+                let animationAdded = scrollers[0].getAttribute("data-animated");
+                if (animationAdded === "false" || animationAdded === null) {
+                    addAnimation();
+
+                    // scroll to the top
+                    window.scrollTo(0, 0);
+
+                    // remove yscroll from the body
+                    document.querySelector('body').classList.remove('yscroll');
+                };
+            } else { // no scrollers present
+                // make sure yscroll is on the document
+                document.querySelector('body').classList.add('yscroll');
+            };
+               
             // remove the class 'small-screen' from the body
             document.querySelector('body').classList.remove('small-screen');
 
-            // scroll to the top
-            window.scrollTo(0, 0);
-        } else {
-            // check if animation has been added
-            // if so remove the animation
-            if (animationAdded === "true") {
-                removeAnimation();
-            };
-            // add the class 'small-screen' to the body
-            document.querySelector('body').classList.add('small-screen');   
-        }
+        };
+
     };
+    
     // run the function on page load
-    go();
+    resizeHandler();    
 
     // run the function on resize
-    window.addEventListener('resize', go);
+    window.addEventListener('resize', resizeHandler);
     
 };
 
@@ -70,7 +100,6 @@ function addDragable() {
             e.preventDefault();
             x = e.offsetX;
             // scrollerInner.style.left;
-            console.log(scrollerInner)
         });
 
 
@@ -83,7 +112,6 @@ function addAnimation() {
 
         const scrollerInner = scroller.querySelector(".scroll__track");        
         const scrollerContent = Array.from(scrollerInner.children);
-        console.log('adding animation');
 
         scrollerContent.forEach(item => {
             const duplicatedItem = item.cloneNode(true);
@@ -94,7 +122,6 @@ function addAnimation() {
         });
     });
 };
-
 
 function removeAnimation() {
     scrollers.forEach(scroller => {
@@ -114,8 +141,6 @@ function removeAnimation() {
 
 
 // Add draggable movement to scroller
-
-
 
 
 // Nav link corresponding to current page stays highlighted
